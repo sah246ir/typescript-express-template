6266e88674e0b9e9eeb89814493359b1a32302ad
sah246ir <sahirchawla2004@gmail.com>
2024-10-16 19:49:10 +0530

    chore: cors headers from env

--- src/Middlewares/cors-headers.ts
+++ src/Middlewares/cors-headers.ts
@@ -1,7 +1,8 @@
 import { NextFunction,Response,Request } from "express";
+import { clients } from "..";
 
 export function corsHeaders(req:Request, res:Response, next:NextFunction){
-    res.header('Access-Control-Allow-Origin','http://url1, http://url2');
+    res.header('Access-Control-Allow-Origin', ...clients);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
